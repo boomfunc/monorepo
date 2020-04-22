510b485fdc5a3aa20bbc23c624cde7f7edf5be19
Alexander Gurinov <alexander.gurinov@gmail.com>
2020-04-22 12:59:01 +0300

    Fix gitea url to rewrite

--- .make/git.mk
+++ .make/git.mk
@@ -4,4 +4,4 @@
 # Tell git to use ssh clone instead https (for private repos).
 # Workaround with invoking at initialization stage of this Makefile.
 GITHUB_REWRITE := $(shell git config --global url.git@github.com:.insteadOf https://github.com/)
-BOOMFUNC_GITEA_REWRITE := $(shell git config --global url.gitea@git.boomfunc.io:.insteadOf https://gitea.boomfunc.io/)
+BOOMFUNC_GITEA_REWRITE := $(shell git config --global url.gitea@gitea.boomfunc.io:.insteadOf https://gitea.boomfunc.io/)
